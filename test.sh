b619e6d2f8014b3b32ee346faf0c86f2f3d78220
alanbasurto <alanrbasurto@gmail.com>
2023-09-26 17:12:05 -0600

    fixing validation 2

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 array=($(grep '"SEVERITY.HIGH":' results.sarif))
 number=(${array[1]:0:$((${#array[1]}-1))})
 if [ $((number)) -eq 0 ];then
- echo "there is noy any issue"
+ echo "there is noy any issue you can go ahead"
  echo "flag=false" >> "$GITHUB_ENV"
  echo "flag set to false"
 else 
