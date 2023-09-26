3558afc81af79bf077f01857c1bf87fe7b8978de
alanbasurto <79418672+alanbasurto@users.noreply.github.com>
2023-09-26 14:49:45 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-array=($(grep '"SEVERITY.HIGH":' data.sarif))
+array=($(grep '"SEVERITY.HIGH":' results.sarif))
 number=(${array[1]:0:$((${#array[1]}-1))})
 if [ $((number)) -gt 0 ];then
 echo "there is a SEVERITY.HIGH"
