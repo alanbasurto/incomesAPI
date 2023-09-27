b7dd2e9e5126be85200804d89e7544586eaefae1
Alan Basurto <alanrbasurto@gmail.com>
2023-09-27 10:43:22 -0600

    testing 4

--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@ number=(${array[1]:0:$((${#array[1]}-1))})
 if [ $((number)) -eq 0 ];then
  echo "noissue"
  echo "flag=false" >> "$GITHUB_ENV"
+ echo "action_stateeee=red" >> "$GITHUB_ENV"
  echo "flag set to false"
 else 
  echo "there is a SEVERITY.HIGH"
  echo "flag=true" >> "$GITHUB_ENV" 
  echo "flag set to true" 
+ echo "action_stateeee=green" >> "$GITHUB_ENV"
 fi
