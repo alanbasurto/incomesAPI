84f420d423c8a262959dfbb1118cae05b1541b9f
Alan Basurto <alanrbasurto@gmail.com>
2023-09-27 10:53:43 -0600

    testing 5.4

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@ array=($(grep '"SEVERITY.HIGH":' results.sarif))
 number=(${array[1]:0:$((${#array[1]}-1))})
 if [ $((number)) -eq 0 ];then
  echo "noissue"
- echo "flag=false" >> "$GITHUB_ENV"
+ echo "FLAG=false" >> "$GITHUB_ENV"
  echo "action_stateeee=red" >> "$GITHUB_ENV"
- echo "flag set to false"
+ echo "FLAG set to false"
 else 
  echo "there is a SEVERITY.HIGH"
- echo "flag=true" >> "$GITHUB_ENV" 
- echo "flag set to true" 
+ echo "FLAG=true" >> "$GITHUB_ENV" 
+ echo "FLAG set to true" 
  echo "action_stateeee=green" >> "$GITHUB_ENV"
 fi
